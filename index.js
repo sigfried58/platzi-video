96a94b9210b40e3a5fbebad6f7dd5216a5c526f6
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-06-30 23:11:43 -0400

    Initial configuration of webpack and react

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const app = document.getElementById('app');
+
+const holaMundo = <h1>hola mundo!</h1>;
+ReactDom.render(holaMundo, app);
