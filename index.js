190774867ef65be72d0d890d9f5aa7acfa5af90d
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-07-01 23:18:52 -0400

    Added list

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Media from './src/playlist/components/media';
+import Playlist from './src/playlist/components/playlist';
+import data from './src/api.json';
 
 const app = document.getElementById('app');
 
-render(
-  <Media
-    type="video"
-    title="¿Qué es responsive Design?"
-    author="LeonidasEsteban"
-    image="./images/covers/responsive.jpg"
-  />,
-  app
-);
+render(<Playlist data={data} />, app);
