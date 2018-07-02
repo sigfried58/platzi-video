52b6b5877f2cd05b65bcc78e63b30a589a0cde36
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-07-01 22:44:35 -0400

    Props and new images

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ import Media from './src/playlist/components/media';
 
 const app = document.getElementById('app');
 
-render(<Media />, app);
+render(
+  <Media
+    title="¿Qué es responsive Design?"
+    author="LeonidasEsteban"
+    image="./images/covers/responsive.jpg"
+  />,
+  app
+);
