347728faca27dc510232c863e5418c6c96589ac7
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-07-09 19:15:06 -0400

    UI forms

--- src/categories/components/categories.js
+++ src/categories/components/categories.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Category from './category';
+import Search from '../../widgets/containers/search';
 import './categories.css';
 
 function Categories(props) {
   return (
     <div className="Categories">
+      <Search />
       {props.categories.map(item => {
         return (
           <Category
---
+++ src/widgets/containers/search.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Search from '../components/search';
+
+class SearchContainer extends Component {
+  render() {
+    return <Search />;
+  }
+}
+
+export default SearchContainer;
