08612c06c15bbdf774731299049ee9e4059e217d
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-07-10 19:55:41 -0400

    Title and Video Source Components

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
+import Video from '../components/video';
+import Title from '../components/title';
 
 class VideoPlayer extends Component {
   render() {
     return (
       <VideoPlayerLayout>
+        <Title title="Esto es un video chido!" />
         <video
-          controls
           autoPlay
           src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
         />
